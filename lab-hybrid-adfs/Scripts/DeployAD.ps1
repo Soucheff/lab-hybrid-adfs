e238ff3bf45cba6e35850df3a3fd886133e34a05
Pedro Soucheff <pedro.soucheff@live.com>
2022-03-24 00:18:01 -0300

    Fix Install ADDSForest

--- lab-hybrid-adfs/Scripts/DeployAD.ps1
+++ lab-hybrid-adfs/Scripts/DeployAD.ps1
@@ -58,7 +58,12 @@ if (!(Test-Path -Path "$($completeFile)$step")) {
                            -IncludeAllSubFeature 
 
     Install-ADDSForest -DomainName $domain `
-                       -Force `
+                       -DomainMode "7" `
+                       -ForestMode "7" ` 
+                       -Force?$true `
+                       -NoRebootOnCompletion:$True ` 
+                       -InstallDns:$true ` 
+                       -CreateDnsDelegation:$false ` 
                        -SafeModeAdministratorPassword $smPassword 
 
     #record that we got this far
