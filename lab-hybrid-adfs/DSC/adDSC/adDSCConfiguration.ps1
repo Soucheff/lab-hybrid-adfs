72f1e0b79c1227f0d8aea966b69d64367bd70e13
Pedro Soucheff <pedro.soucheff@live.com>
2022-03-24 20:47:28 -0300

    DSC Fix

--- lab-hybrid-adfs/DSC/adDSC/adDSCConfiguration.ps1
+++ lab-hybrid-adfs/DSC/adDSC/adDSCConfiguration.ps1
@@ -206,30 +206,6 @@ configuration DomainController
 			}
 		}
 
-        Script InstallAADConnect
-        {
-            SetScript = {
-                $AADConnectDLUrl="https://download.microsoft.com/download/B/0/0/B00291D0-5A83-4DE7-86F5-980BC00DE05A/AzureADConnect.msi"
-                $exe="$env:SystemRoot\system32\msiexec.exe"
-
-                $tempfile = [System.IO.Path]::GetTempFileName()
-                $folder = [System.IO.Path]::GetDirectoryName($tempfile)
-
-                $webclient = New-Object System.Net.WebClient
-                $webclient.DownloadFile($AADConnectDLUrl, $tempfile)
-
-                Rename-Item -Path $tempfile -NewName "AzureADConnect.msi"
-                $MSIPath = $folder + "\AzureADConnect.msi"
-
-                Invoke-Expression "& `"$exe`" /i $MSIPath /qn /passive /forcerestart"
-            }
-
-            GetScript =  { @{} }
-            TestScript = { 
-                return Test-Path "$env:TEMP\AzureADConnect.msi" 
-            }
-        }
-
         Script CreateOU
         {
             SetScript = {
